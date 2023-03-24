41d9d9028059d219ba7b31508758458e36faeed2
Carlo-Mantovani <carlomanto88@gmail.com>
2023-03-24 01:46:29 -0300

    feat: Cost calculation between cities for different vehicle types

--- App.java
+++ App.java
@@ -5,42 +5,37 @@ import java.util.Map;
 
 public class App {
 
+    
     public static void main(String[] args) {
 
         Scanner menu = new Scanner(System.in);
         ReadFile fr = new ReadFile();
-        String file = "DNIT-Distancias.csv";
+        String data = "DNIT-Distancias.csv";
         Map<String,Map<String,Integer>> routes = new HashMap<String,Map<String,Integer>>();
-        fr.readFile(file, routes);
-        //for (Origin origin : origins) {
-        //    System.out.println(origin.getName());
-        //    for (Map.Entry<String, Integer> entry : origin.getDest_distance().entrySet()) {
-        //        System.out.println(entry.getKey() + " " + entry.getValue());
+        fr.readFile(data, routes);
+
+        //for (Map.Entry<String, Map<String, Integer>> entry : routes.entrySet()) {
+        //    System.out.println(entry.getKey());
+        //    for (Map.Entry<String, Integer> entry2 : entry.getValue().entrySet()) {
+        //        System.out.println(entry.getKey() + " " + entry2.getKey() + " " + entry2.getValue());
         //    }
         //}
-        for (Map.Entry<String, Map<String, Integer>> entry : routes.entrySet()) {
-            System.out.println(entry.getKey());
-            for (Map.Entry<String, Integer> entry2 : entry.getValue().entrySet()) {
-                System.out.println(entry.getKey() + " " + entry2.getKey() + " " + entry2.getValue());
-            }
+        Map<Integer, Double> costs = new HashMap<Integer, Double>();
+        String costFile = "custos.txt";
+        fr.readCost(costFile, costs);
+        for (Map.Entry<Integer, Double> entry : costs.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
 
-        // for (Map.Entry<Integer, String> entry : destinations.getDest().entrySet()) {
-        // System.out.println(entry.getKey() + " " + entry.getValue());
-        // }
-        //for (Route route : routes) {
-        //    System.out.println(route.getName() + " " + destinations.getDest().get(route.getDest_id()) + " "
-        //            + route.getDest_distance());
-        //}
-
-        // System.out.print("\f");
+     
         System.out.print("\nBem-vindo!\n");
 
         // O que estabelece o loop + o menu
         while (true) {
             System.out.print("\n");
             System.out.print("|-----------------------------------------------|\n");
-            System.out.print("| Opcao 1 - Sair                                |\n");
+            System.out.print("| Opcao 1 - Consultar Trechos x Modalidade      |\n");
+            System.out.print("| Opcao 2 - Sair                                |\n");
             System.out.print("|-----------------------------------------------|\n");
             System.out.print("Selecione uma opcao: ");
 
@@ -48,7 +43,58 @@ public class App {
 
             switch (opt) {
 
-                case 1:
+                case 1: 
+                    System.out.print("\n");
+                    System.out.print("Digite a cidade de origem: \n");
+                    menu.nextLine();
+                    String origin = menu.nextLine();
+
+                    origin = origin.toUpperCase();
+
+                    if (!routes.containsKey(origin)){
+
+                        System.out.println("Cidade de origem nao encontrada");
+                        break;
+                    }
+                    System.out.print("Digite a cidade de destino: \n");
+                    String destination = menu.nextLine();
+                    destination = destination.toUpperCase();
+
+                    if (!routes.get(origin).containsKey(destination)){
+                        System.out.println("\nCidade de destino nao encontrada");
+                        break;
+                    }
+                    
+                    System.out.print("Digite o tipo de veiculo: \n");
+                    System.out.print("1 - Pequeno Porte\n");
+                    System.out.print("2 - Medio Porte\n");
+                    System.out.print("3 - Grande Porte\n");
+                    int vehicle_type = menu.nextInt();
+                    
+                    destination.toUpperCase();
+                    String cost;
+                    switch (vehicle_type) {
+                        case 1:
+                            cost = String.format("%.2f",(costs.get(1) * routes.get(origin).get(destination)));
+                            System.out.print("O custo do trecho é: R$" + cost);
+                            break;
+                        case 2:
+                            cost = String.format("%.2f",(costs.get(2) * routes.get(origin).get(destination)));
+                            System.out.print("O custo do trecho é: R$" + cost);
+                            break;
+                        case 3:
+                            cost = String.format("%.2f",(costs.get(3) * routes.get(origin).get(destination)));
+                            System.out.print("O custo do trecho é: R$" + cost);
+                            break;
+                        default:
+                            System.out.print("Opçao invalida");
+                            break;
+                    }
+
+
+                    break;
+
+                case 2:
                     System.exit(0);
                     menu.close();
 
--- ReadFile.java
+++ ReadFile.java
@@ -48,4 +48,21 @@ public class ReadFile {
         }
 
     }
+    //le o arquivo de custos, e adiciona no map seguindo o padrão de id e custo
+    // Id = 1 -> Pequeno Porte
+    // Id = 2 -> Médio Porte
+    // Id = 3 -> Grande Porte
+    public void readCost(String file, Map <Integer, Double> costs) {
+        Path path = Paths.get(file);
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String[] aux = line.split(";");
+                costs.put(Integer.parseInt(aux[0]), Double.parseDouble(aux[1]));
+            }
+
+        } catch (IOException e) {
+            System.err.format("Error", e);
+        }
+    }
 }
